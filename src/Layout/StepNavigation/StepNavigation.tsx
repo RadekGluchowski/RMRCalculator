54f667f062f696a664442369f8e6687d5b4e00e6
Radoslawgluchowski_1@wp.pl <Radoslawgluchowski_1@wp.pl>
2021-09-15 17:13:56 +0200

    add stepNavigation component with styles.

--- src/Layout/StepNavigation/StepNavigation.tsx
+++ src/Layout/StepNavigation/StepNavigation.tsx
@@ -1,11 +1,20 @@
 import React from "react";
 import { StepWizardChildProps } from "react-step-wizard";
+import "./StepNavigation.css"
 
-export const StepNavigation: React.FC<Partial<StepWizardChildProps>> = ({ ...props }) => {
-
+export const StepNavigation: React.FC<Partial<StepWizardChildProps>> = ({ totalSteps, currentStep, goToStep }) => {
+    const handleNavigation = (stepNumber: number) => {
+        if (goToStep) {
+            goToStep(stepNumber + 1)
+        }
+    }
     return (
-        <div >
-
+        <div className="nav-dot-container">
+            <ol>
+                {[...Array(totalSteps)].map((x, i) =>
+                    <li key={i} onClick={() => handleNavigation(i)} className={currentStep === i + 1 ? "current" : ""}>Step {i + 1}</li>
+                )}
+            </ol>
         </div>
     );
 }
