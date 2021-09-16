5b5b667ff991f0c8813da50f771bf57ded128a6b
Radoslawgluchowski_1@wp.pl <Radoslawgluchowski_1@wp.pl>
2021-09-16 15:41:50 +0200

    add stepOne reducer to root reducers.

--- src/Store/Reducers/rootReducer.ts
+++ src/Store/Reducers/rootReducer.ts
@@ -1,8 +1,9 @@
 
 import { combineReducers } from 'redux';
+import { stepReducer } from './StepReducer/step.reducer';
 
 export const rootReducer = combineReducers({
-
+    stepOneUserData: stepReducer
 });
 
 export type AppState = ReturnType<typeof rootReducer>;
