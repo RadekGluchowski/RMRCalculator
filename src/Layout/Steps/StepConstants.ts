c5fa12e9f2f4c1a558459acc6653225dec46aa4d
Radoslawgluchowski_1@wp.pl <Radoslawgluchowski_1@wp.pl>
2021-09-18 11:23:49 +0200

    export step enums, created step constants.

---
+++ src/Layout/Steps/StepConstants.ts
@@ -0,0 +1,58 @@
+export const ActivitiesData = {
+    AerobicIntensive: {
+        name: "Aerobics (intensive)",
+        kcalPerHour: 520
+    },
+    Aerobic: {
+        name: "Aerobics",
+        kcalPerHour: 400
+    },
+    Badminton: {
+        name: "Badminton",
+        kcalPerHour: 370
+    },
+    Boxing: {
+        name: "Boxing (sparing)",
+        kcalPerHour: 865
+    },
+    CyclingIntensive: {
+        name: "Cycling (16km/h +)",
+        kcalPerHour: 385
+    },
+    Cycling: {
+        name: "Cycling",
+        kcalPerHour: 250
+    },
+    Judo: {
+        name: "Judo",
+        kcalPerHour: 760
+    },
+    Rowing: {
+        name: "Rowing",
+        kcalPerHour: 445
+    },
+    RunningIntensive: {
+        name: "Running (3.8 min/km)",
+        kcalPerHour: 1000
+    },
+    Running: {
+        name: "Running",
+        kcalPerHour: 750
+    },
+    Squash: {
+        name: "Squash",
+        kcalPerHour: 615
+    },
+    Swimming: {
+        name: "Swimming",
+        kcalPerHour: 630
+    },
+    Tenis: {
+        name: "Tenis",
+        kcalPerHour: 415
+    },
+    StrengthExercises: {
+        name: "Strength Exercises",
+        kcalPerHour: 360
+    }
+}
---
+++ src/Layout/Steps/StepEnums.ts
@@ -0,0 +1,39 @@
+export enum StepOneForm {
+    STEP = "Step",
+    WEIGHT_FIELD_LABLE = "Weight",
+    WEIGHT_FIELD_NAME = "weight",
+    WEIGHT_FIELD_ID = "weight",
+    AGE_FIELD_LABEL = "Age",
+    AGE_FIELD_ID = "age",
+    AGE_FIELD_NAME = "age",
+    AGE_YOUNG = "10 - 18",
+    AGE_MIDDLE = "18 - 30",
+    AGE_OLD = "31 - 60+",
+    SEX_FIELD_LABEL = "Sex",
+    SEX_FIELD_ID = "sex",
+    SEX_FIELD_NAME = "sex",
+    PERSON_TYPE_FIELD_ID = "personType",
+    PERSON_TYPE_FIELD_NAME = "personType",
+    PERSON_TYPE_FIELD_LABEL = "Type of person",
+    NEXT_STEP_BTN_TEXT = "Next Step",
+    SEX_MALE = "Male",
+    SEX_FEMALE = "Female",
+    PERSON_TYPE_LOW_ACTIVE_NAME = "Low Active",
+    PERSON_TYPE_LOW_ACTIVE_VALUE = "lowActive",
+    PERSON_TYPE_MEDIUM_ACTIVE_NAME = "Medium Active",
+    PERSON_TYPE_MEDIUM_ACTIVE_VALUE = "mediumActive",
+    PERSON_TYPE_HIGHT_ACTIVE_NAME = "Hight Active",
+    PERSON_TYPE_HIGHT_ACTIVE_VALUE = "hightActive"
+}
+
+export enum StepTwoForm {
+    PREVIOUS_STEP_BTN_TEXT = "Previous Step",
+    ACTIVITY_TYPE = "Activity Type",
+    ACTIVITY_TYPE_ID = "activityType",
+    TIME_FIELD = "Time in hours",
+    CALCULATE_BTN_NAME = "Calculate"
+}
+
+export enum StepShared {
+    SELECT_FIELD_TYPE = "select",
+}
