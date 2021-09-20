d7c0ff22e37825850c83f285cc11fe09aa1534e6
Radoslawgluchowski_1@wp.pl <Radoslawgluchowski_1@wp.pl>
2021-09-20 13:57:23 +0200

    add stepSummary component

---
+++ src/Layout/Steps/StepSummary/StepSummary.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { StepWizardChildProps } from "react-step-wizard";
+
+
+export const StepSummary: React.FC<Partial<StepWizardChildProps>> = ({ }) => {
+
+
+    return (
+        <>
+            lol
+        </>
+    );
+}
+
+export default StepSummary;
