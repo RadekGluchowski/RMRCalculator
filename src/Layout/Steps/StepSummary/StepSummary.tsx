8949e21c0a48ca67cc9f896c33d64a02207f5678
Radoslawgluchowski_1@wp.pl <Radoslawgluchowski_1@wp.pl>
2021-09-22 18:26:30 +0200

    basic display of calculations.

--- src/Layout/Steps/StepSummary/StepSummary.tsx
+++ src/Layout/Steps/StepSummary/StepSummary.tsx
@@ -1,15 +1,18 @@
 import React from "react";
 import { StepWizardChildProps } from "react-step-wizard";
 import { useSelector } from "react-redux";
-import { selectSummary, selectSummaryCarbs, selectProteinsInGrams, selectProteinsPercentOfTotalKcal } from "../../../Store/Selectors/Selectors";
+import { selectSummary, selectCarbsInGrams, selectProteinsInGrams, selectProteinsPercentOfTotalKcal, selectFatPercentOfTotalKcal, selectFatInGrams } from "../../../Store/Selectors/Selectors";
 import { AppState } from "../../../Store/Reducers/rootReducer";
 
 
 export const StepSummary: React.FC<Partial<StepWizardChildProps>> = ({ }) => {
     const summaryKcal = useSelector<AppState>(selectSummary);
-    const carbs = useSelector<AppState>(selectSummaryCarbs);
     const proteinsInGrams = useSelector<AppState>(selectProteinsInGrams);
     const proteinsPercentOfTotalKcal = useSelector<AppState>(selectProteinsPercentOfTotalKcal)
+    const fatPercentOfTotalKcal = useSelector<AppState>(selectFatPercentOfTotalKcal)
+    const fatInGrams = useSelector<AppState>(selectFatInGrams)
+    const carbsInGrams = useSelector<AppState>(selectCarbsInGrams);
+    const carbsPercentOfTotalKcal = 60;
 
     return (
         <div>
@@ -17,13 +20,24 @@ export const StepSummary: React.FC<Partial<StepWizardChildProps>> = ({ }) => {
                 <p>  {summaryKcal}</p>
             </div>
             <>
-                <p>  {carbs}</p>
+                <p>  {carbsInGrams}</p>
+            </>
+            <>
+                <p>  {carbsPercentOfTotalKcal}</p>
             </>
             <>
                 <p> {proteinsInGrams} </p>
             </>
             <>
                 <p> {proteinsPercentOfTotalKcal} </p>
+            </>
+
+            <>
+                <p> {fatPercentOfTotalKcal} </p>
+            </>
+
+            <>
+                <p> {fatInGrams} </p>
             </>
         </div>
     );
